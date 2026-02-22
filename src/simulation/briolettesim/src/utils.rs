e5d38cc7532b9a7bea085d795cf08dad54e18bf5
Panagiotis Michalopoulos <11323100+pan-m@users.noreply.github.com>
2026-02-22 04:02:11 -0500

    Ensure there are always at least two double-spenders

--- src/simulation/briolettesim/src/utils.rs
+++ src/simulation/briolettesim/src/utils.rs
@@ -2,14 +2,11 @@ pub fn split_population(
     total_agents_in_population: u64,
     bad_agents_to_population_ratio: f64, 
 ) -> (usize, usize) {
-    let bad_actors: usize = (
+    let bad_actors: usize = 1.max((
         (total_agents_in_population as f64) * 
         (bad_agents_to_population_ratio / 2.0)
-    ).floor() as usize;
-    let good_actors: usize = (
-        (total_agents_in_population as f64) * 
-        (1.0 - bad_agents_to_population_ratio)
-    ).floor() as usize;
+    ).floor() as usize);
+    let good_actors: usize = total_agents_in_population as usize - 2 * bad_actors;
 
     (good_actors, bad_actors) 
 }
