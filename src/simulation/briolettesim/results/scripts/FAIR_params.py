438f57a0c0501a6d6ebf78feb50d69274f6c5b96
Panagiotis Michalopoulos <11323100+pan-m@users.noreply.github.com>
2026-03-25 02:32:54 -0400

    Add semi-realistic set of parameters and implement minor fixes

--- src/simulation/briolettesim/results/scripts/FAIR_params.py
+++ src/simulation/briolettesim/results/scripts/FAIR_params.py
@@ -6,30 +6,34 @@ names_4 = ['move_prob', 'p2p', 'p2m', 'ds_ratio']
 bounds_4 = [[0.05, 0.7],
             [0.1, 0.5],
             [0.1, 0.5],
-            [0.05264, 3]] # 1 for every 20 -> 3 for 1 (Malicious:Honest)
+            [0.05264, 3]] # 1 for every 19 -> 3 for 1 (Malicious:Honest)
 bounds_4_realistic_adversary = [[0.05, 0.7],
             [0.1, 0.5],
             [0.1, 0.5],
-            [0.0001, 0.01]] # 1 in every 10k -> 1 in every 100 (Malicious:Honest)
-bounds_bottom_10_4 = [[0.05332457115873695, 0.6976294308900832],
-            [0.10273502804338933, 0.49859678372740746],
-            [0.1007303286343813, 0.4989615138620138],
-            [0.054364249609559774, 0.22308683157727122]]
-bounds_top_10_4 = [[0.05313667901791633, 0.6997700172010809],
-            [0.10036114566028119, 0.4984994933009148],
-            [0.10007842518389226, 0.4978264268487692],
-            [2.100510159525573, 2.999915667119175]]
+            [0.0002, 0.02]] # 2 for every 10k -> 2 for every 100 (Malicious:Honest)
+bounds_4_semi_realistic_adversary = [[0.05, 0.7],
+            [0.1, 0.5],
+            [0.1, 0.5],
+            [0.002, 1]] # 2 for every 1k -> 1 for every 1 (Malicious:Honest)
+# bounds_bottom_10_4 = [[0.05332457115873695, 0.6976294308900832],
+#             [0.10273502804338933, 0.49859678372740746],
+#             [0.1007303286343813, 0.4989615138620138],
+#             [0.054364249609559774, 0.22308683157727122]]
+# bounds_top_10_4 = [[0.05313667901791633, 0.6997700172010809],
+#             [0.10036114566028119, 0.4984994933009148],
+#             [0.10007842518389226, 0.4978264268487692],
+#             [2.100510159525573, 2.999915667119175]]
 
 problem = {
     'num_vars': 4,
     'names': names_4,
-    'bounds': bounds_4_realistic_adversary
+    'bounds': bounds_4_semi_realistic_adversary
 }
 
 sp = ProblemSpec(problem)
 sp.sample_sobol(1024, calc_second_order=False)
 
-np.savetxt(f"results/FAIR_params_{problem["num_vars"]}_realistic/FAIR_params.txt", sp.samples, "%.17g")
+np.savetxt(f"results/FAIR_params_{problem["num_vars"]}_semi-realistic_1b/FAIR_params.txt", sp.samples, "%.17g")
 
 # Old way
 # param_values = sobol.sample(problem, 1024, calc_second_order=False)
