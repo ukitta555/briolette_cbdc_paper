6ae1996cd5c364c06c6a9bf4c414de38b4722c24
Panagiotis Michalopoulos <11323100+pan-m@users.noreply.github.com>
2026-05-29 11:26:41 -0400

    Change to 20 offline transactions

--- src/simulation/briolettesim/src/main.rs
+++ src/simulation/briolettesim/src/main.rs
@@ -1167,8 +1167,8 @@ fn main() -> io::Result<()> {
                                     p2m_probability: *p2m_probability, 
                                     p2p_probability: *p2p_probability, 
                                     move_probability: *move_probability,   
-                                    merchant_sync_frequency: 12, // original value 8
-                                    tickets_given_right_away: 12, //original value 8 
+                                    merchant_sync_frequency: 20, // original value 8
+                                    tickets_given_right_away: 20, //original value 8 
                                     tickets_lower_bound_to_sync: 1, //original value 2
                                     account_balance: 200,
                                     model: Model::Rural,
